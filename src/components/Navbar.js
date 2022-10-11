8c5fcc048be43339dccd4b71106f0fc7393f8131
martin.kratochvil <martvil96@gmail.com>
2022-10-11 14:44:57 +0200

    added about

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export default function Navbar({ active, logout }) {
-
-    const activeStyles = "block py-2 pr-4 pl-3 text-white bg-blue-700 rounded bg-transparent text-blue-700 p-0 dark:text-white";
-    const nonActiveStyles = "block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 hover:bg-transparent hover:text-blue-700 p-0 dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:hover:bg-transparent dark:border-gray-700";
+    
+    const activeStyles = "block p-2 sm:p-4 text-white bg-blue-700 rounded bg-transparent text-blue-700 dark:text-white";
+    const nonActiveStyles = "block p-2 sm:p-4 text-gray-700 rounded hover:bg-gray-100 hover:bg-transparent hover:text-blue-700 dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:hover:bg-transparent dark:border-gray-700";
 
   return (
     <div>
@@ -27,13 +27,18 @@ export default function Navbar({ active, logout }) {
                         </a>
                     </li>
                     <li>
-                    <button
-                        type="submit"
-                        className="flex flex-row px-5 py-2 text-blue-600 border-solid border-blue-600 border-[1px] bg-white font-medium text-sm leading-snug uppercase rounded whitespace-nowrap shadow-md hover:bg-blue-700 hover:text-white hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                        onClick={logout}
-                        >
-                        Logout
-                    </button>
+                        <a href="investments" className={active === "investments" ? activeStyles : nonActiveStyles}>
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <button
+                            type="submit"
+                            className="flex flex-row px-5 py-2 text-blue-600 border-solid border-blue-600 border-[1px] bg-white font-medium text-sm leading-snug uppercase rounded whitespace-nowrap shadow-md hover:bg-blue-700 hover:text-white hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                            onClick={logout}
+                            >
+                            Logout
+                        </button>
                     </li>
                 </ul>
             </div>
