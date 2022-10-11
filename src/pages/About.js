eab6a0f0f441acf72cf6314b371426f5f8cf3588
martin.kratochvil <martvil96@gmail.com>
2022-10-11 15:24:15 +0200

    added about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,20 +12,23 @@ export default function About() {
                 <span className='text-blue-600'>ADD</span> STOCKS AND <span className='text-blue-600'>MANAGE</span> YOUR STOCK PORTFOLIO
             </h1>
             <p className='max-w-[60%] text-xl'>
-                Under the section <a href="/stocks" className='text-blue-600'>Stocks</a> add the stocks you have in your portfolio. Specify ticker and amount. App uses Yahoo Finance API, so in 
-                order to add your stocks correctly, refer to <a href="https://finance.yahoo.com/" className='text-blue-600'>https://finance.yahoo.com/</a> and 
+                Under the section <a href="/stocks" className='text-blue-600 underline underline-offset-4'>Stocks</a> add the stocks you have in your portfolio. Specify ticker and amount. App uses Yahoo Finance API, so in 
+                order to add your stocks correctly, refer to <a href="https://finance.yahoo.com/" className='text-blue-600 underline underline-offset-4'>https://finance.yahoo.com/</a> and 
                 search for corresponding tickers (eg. 'AAPL' or 'MSFT').
             </p>
             <p className='max-w-[60%] text-xl'>
                 Every day the server looks for previous close price for each stock in your portfolio and updates current net-worth.
-                Financials (net-worth history and pie chart of all your stocks) are displayed under the <a href="/charts" className='text-blue-600'>Charts</a> section.
+                Financials (net-worth history and pie chart of all your stocks) are displayed under the <a href="/charts" className='text-blue-600 underline underline-offset-4'>Charts</a> section.
             </p>
             <p className='max-w-[60%] text-xl'>
-                To get a rough idea about how much you invest and what is your discipline when investing, refer to the <a href="/investments" className='text-blue-600'>Investments </a>
+                To get a rough idea about how much you invest and what is your discipline when investing, refer to the <a href="/investments" className='text-blue-600 underline underline-offset-4'>Investments </a>
                 section, where the amount of money you invested is displayed. 
             </p>
             <p className='max-w-[60%] text-xl'>
                 This app is intended for the 'Bogglehead' type investor who likes to invest regularly over longer periods of time.
+            </p>
+            <p className='max-w-[60%] text-xl pt-24'>
+                <a href="https://github.com/MartinKratochvilProgramy/Portfolio_management" className='text-blue-600 underline underline-offset-4'>GitHub</a>
             </p>
         </div>
     </div>
